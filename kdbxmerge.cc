fb11c415d15ac591953f0341245d501055d8585c
Opalraava <opalraava@riseup.net>
2018-02-22 13:21:21 +0100

    bugfixes

--- kdbxmerge.cc
+++ kdbxmerge.cc
@@ -4,6 +4,7 @@
 #include <boost/version.hpp>
 #include <boost/algorithm/string.hpp>
 
+void usage(void);
 
 int
 main(int argc, char* argv[])
@@ -15,5 +16,18 @@ main(int argc, char* argv[])
     << BOOST_VERSION % 100		 // patch level
     << std::endl;
 
+
+  usage();
+  
   return 0;
+}
+
+void usage(void)
+{
+  std::cout
+    << "Usage: kdbxmerge [-p password] [-k keyfile] input.kdbx... [-p password] [-k keyfile] output.kdbx\n"
+    << "\n"
+    << "Example: kdbxmerge -p a a.kdbx -p b b.kdbx -p c c.kdbx -p target target.kdbx\n"
+    ;
+  
 }
