fc34ddfd5c5999b8b279c8d62f82167701104aa5
Opalraava <opalraava@valkyrie.fritz.box>
2018-02-22 11:25:31 +0100

    bugfixes

---
+++ kdbxmerge.cc
@@ -0,0 +1,19 @@
+#include <config.h>
+
+#include <iostream>
+#include <boost/version.hpp>
+#include <boost/algorithm/string.hpp>
+
+
+int
+main(int argc, char* argv[])
+{
+  std::cout
+    << "Using Boost "     
+    << BOOST_VERSION / 100000     << "." // major version
+    << BOOST_VERSION / 100 % 1000 << "." // minor version
+    << BOOST_VERSION % 100		 // patch level
+    << std::endl;
+
+  return 0;
+}
